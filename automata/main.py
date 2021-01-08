85806b73ff20e9b72e48d7790601198a55d06a43
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-01-07 22:00:51 -0300

    Run automaton on a given event sequence

--- automata/main.py
+++ automata/main.py
@@ -13,10 +13,12 @@ def main():
     initial_state = '0'
     marked_states = ['1']
     alphabet = ['a','b']
+    event_sequence = 'aaba'
 
     dfa = DeterministicFiniteAutomaton(states, transitions, initial_state, marked_states, alphabet)
 
     dfa.print_automaton()
+    dfa.run(event_sequence)
 
 if __name__ == '__main__':
 
