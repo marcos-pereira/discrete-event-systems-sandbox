21b138cbf47b2f8602d00f91665084ede3c1175f
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-01-13 21:56:35 -0300

    Plot automaton

--- automata/main.py
+++ automata/main.py
@@ -5,10 +5,11 @@ from DeterministicFiniteAutomaton import DeterministicFiniteAutomaton
 def main():
     print("main()...")
 
-    states = ['0','1']
+    states = ['0','1','2']
     transitions = {
         '0':{'a':'1','b':'0'},
-        '1':{'a':'1','b':'0'}
+        '1':{'a':'2','b':'0'},
+        '2':{}
     }
     initial_state = '0'
     marked_states = ['1']
@@ -19,6 +20,7 @@ def main():
 
     dfa.print_automaton()
     dfa.run(event_sequence)
+    dfa.plot_automaton('automaton1')
 
 if __name__ == '__main__':
 
