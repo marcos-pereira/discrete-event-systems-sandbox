d491482ae41995e7984ff638e7b6781a640b0749
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-01-15 21:36:29 -0300

    Plot automaton after removing unreachable states

--- automata/main.py
+++ automata/main.py
@@ -5,7 +5,7 @@ from DeterministicFiniteAutomaton import DeterministicFiniteAutomaton
 def main():
     print("main()...")
 
-    states = ['0','1','2']
+    states = {'0','1','2','3'}
     transitions = {
         '0':{'a':'1','b':'0'},
         '1':{'a':'2','b':'0'},
@@ -13,16 +13,24 @@ def main():
         '3':{'a':'2'}
     }
     initial_state = '0'
-    marked_states = ['1']
-    alphabet = ['a','b']
+    marked_states = {'2'}
+    alphabet = {'a','b'}
     event_sequence = 'aaba'
 
     dfa = DeterministicFiniteAutomaton(states, transitions, initial_state, marked_states, alphabet)
 
+    ## Show automaton plot output
+    show_output = True
+
+    ## Print automaton
     dfa.print_automaton()
     dfa.run(event_sequence)
-    dfa.plot_automaton('automaton1')
+    dfa.plot_automaton('automaton1', show_output)
+
+    ## Convert automaton to get accessible part
     dfa.reachable_automaton()
+    dfa.print_automaton()
+    dfa.plot_automaton('reachable_automaton', show_output)
 
 if __name__ == '__main__':
 
