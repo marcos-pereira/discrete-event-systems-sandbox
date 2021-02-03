94762fdc80d615a0bb6ad74ec0291278bcf40548
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-02-03 19:33:54 -0300

    Add alternative constructor to PetriNet

--- petri_nets/PetriNet.py
+++ petri_nets/PetriNet.py
@@ -6,6 +6,18 @@ class PetriNet:
         self.init_marking_ = init_marking
         self.incidence_matrix_ = incidence_matrix
 
+    @classmethod
+    def split_incidence_matrix(cls, places, init_marking, Aminus, Aplus):
+        incidence_matrix = Aminus+Aplus
+        net = cls(places, init_marking, incidence_matrix)
+        return net
+
+    @classmethod
+    def incidence_matrix(cls, places, init_marking, incidence_matrix):
+        net = cls(places, init_marking, incidence_matrix)
+        return net
+
+
     def next_marking(self, u):
         marking = self.init_marking_ + np.matmul(u,self.incidence_matrix_)
         return marking
