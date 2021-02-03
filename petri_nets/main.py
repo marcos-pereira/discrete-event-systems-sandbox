6742878bd1abd0c9d8f9a2525dac6d2436fccbdc
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-02-03 19:34:10 -0300

    Test PetriNet with Aminus Aplus matrices

--- petri_nets/main.py
+++ petri_nets/main.py
@@ -27,6 +27,23 @@ def main():
     net2_u1 = np.array([1, 0, 0, 0, 0])
     print(net2.next_marking(net2_u1))
 
+    ## Machine 1, Robot and Machine 2 net with Aminus Aplus
+    net3_places = {'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7'}
+    net3_init_marking = np.array([0, 0, 0, 0, 1, 1, 1])
+    net3_Aminus = np.array([[0, 0, 0, 0, -1, 0, 0],
+                            [-1, 0, 0, 0, 0, 0, 0],
+                            [0, -1, 0, 0, 0, -1, 0],
+                            [0, 0, -1, 0, 0, 0, -1],
+                            [0, 0, 0, -1, 0, 0, 0]])
+    net3_Aplus = np.array([[1, 0, 0, 0, 0, 0, 0],
+                           [0, 1, 0, 0, 0, 0, 0],
+                           [0, 0, 1, 0, 1, 0, 0],
+                           [0, 0, 0, 1, 0, 1, 0],
+                           [0, 0, 0, 0, 0, 0, 1]])
+    net3 = PetriNet.split_incidence_matrix(net3_places, net3_init_marking, net3_Aminus, net3_Aplus)
+    net3_u1 = np.array([1, 0, 0, 0, 0])
+    print(net3.next_marking(net3_u1))
+
 if __name__ == '__main__':
 
     main()
