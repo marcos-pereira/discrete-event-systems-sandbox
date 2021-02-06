8583aef3730598e3e2bec95f51a97dc4b0c2aced
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-02-06 20:40:24 -0300

    Test to plot net

--- petri_nets/main.py
+++ petri_nets/main.py
@@ -5,13 +5,17 @@ from PetriNet import PetriNet
 
 def main():
 
+    ## Show output of plotting net
+    show_output = True
+
     ## Client queue and server net
-    net1_places = {'P1','P2','P3'}
+    net1_places = ['P1','P2','P3']
+    net1_transitions = ['t1','t2','t3']
     net1_init_marking = np.array([0,0,1])
     net1_incidence_matrix = np.array([[1,0,0],
                                       [-1,1,-1],
                                       [0,-1,1]])
-    net1 = PetriNet(net1_places,net1_init_marking)
+    net1 = PetriNet(net1_places, net1_transitions, net1_init_marking)
     net1.set_incidence_matrix(net1_incidence_matrix)
     net1.set_arcs()
     # net1.print()
@@ -19,14 +23,15 @@ def main():
     print(net1.next_marking(net1_u1))
 
     ## Machine 1, Robot and Machine 2 net
-    net2_places = {'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7'}
+    net2_places = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7']
+    net2_transitions = ['a1', 'e1', 'd1', 'a2', 'd2']
     net2_init_marking = np.array([0, 0, 0, 0, 1, 1, 1])
     net2_incidence_matrix = np.array([[1, 0, 0, 0, -1, 0, 0],
                                       [-1, 1, 0, 0, 0, 0, 0],
                                       [0, -1, 1, 0, 1, -1, 0],
                                       [0, 0, -1, 1, 0, 1, -1],
                                       [0, 0, 0, -1, 0, 0, 1]])
-    net2 = PetriNet(net2_places, net2_init_marking)
+    net2 = PetriNet(net2_places, net2_transitions, net2_init_marking)
     net2.set_incidence_matrix(net2_incidence_matrix)
     net2.set_arcs()
     # net2.print()
@@ -34,7 +39,8 @@ def main():
     print(net2.next_marking(net2_u1))
 
     ## Machine 1, Robot and Machine 2 net with Aminus Aplus
-    net3_places = {'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7'}
+    net3_places = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7']
+    net3_transitions = ['a1', 'e1', 'd1', 'a2', 'd2']
     net3_init_marking = np.array([0, 0, 0, 0, 1, 1, 1])
     net3_Aminus = np.array([[0, 0, 0, 0, -1, 0, 0],
                             [-1, 0, 0, 0, 0, 0, 0],
@@ -46,13 +52,14 @@ def main():
                            [0, 0, 1, 0, 1, 0, 0],
                            [0, 0, 0, 1, 0, 1, 0],
                            [0, 0, 0, 0, 0, 0, 1]])
-    net3 = PetriNet(net3_places, net3_init_marking, net3_Aminus, net3_Aplus)
+    net3 = PetriNet(net3_places, net3_transitions, net3_init_marking, net3_Aminus, net3_Aplus)
     net3.set_arcs()
     net3.print()
+    net3.plot('net3', show_output)
     net3_u1 = np.array([1, 0, 0, 0, 0])
     print(net3.next_marking(net3_u1))
 
-    
+
 
 if __name__ == '__main__':
 
