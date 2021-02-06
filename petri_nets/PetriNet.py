b3123d335caf6415a2588e3548de69eec6a7441d
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-02-06 16:56:32 -0300

    Initialize class either with Aminus and Aplus or with none. If none, then set incidence matrix with class method.

--- petri_nets/PetriNet.py
+++ petri_nets/PetriNet.py
@@ -1,23 +1,24 @@
 import numpy as np
 
 class PetriNet:
-    def __init__(self, places, init_marking, incidence_matrix):
+
+    incidence_matrix_ = np.array([])
+
+    def __init__(self, places, init_marking, Aminus = np.array([]), Aplus = np.array([])):
         self.places_ = places
         self.init_marking_ = init_marking
+        self.Aminus_ = Aminus
+        self.Aplus_ = Aplus
+        self.incidence_matrix_ = Aminus + Aplus
+
+    def set_incidence_matrix(self, incidence_matrix):
         self.incidence_matrix_ = incidence_matrix
 
-    @classmethod
-    def split_incidence_matrix(cls, places, init_marking, Aminus, Aplus):
-        incidence_matrix = Aminus+Aplus
-        net = cls(places, init_marking, incidence_matrix)
-        return net
-
-    @classmethod
-    def incidence_matrix(cls, places, init_marking, incidence_matrix):
-        net = cls(places, init_marking, incidence_matrix)
-        return net
-
-
     def next_marking(self, u):
-        marking = self.init_marking_ + np.matmul(u,self.incidence_matrix_)
+        if len(self.incidence_matrix_) == len(u):
+            print("Next marking:")
+            marking = self.init_marking_ + np.matmul(u,self.incidence_matrix_)
+        else:
+            print("Incidence matrix not initialized. Returning initial marking:")
+            marking = self.init_marking_
         return marking
