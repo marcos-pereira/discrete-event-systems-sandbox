c93a424d55d859680ca4c50af0baa29e9e7eb55f
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-03-21 20:31:54 -0300

    Test tokens type

--- petri_nets/main_final_project.py
+++ petri_nets/main_final_project.py
@@ -12,6 +12,7 @@ def main():
     net5_places = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8']
     net5_transitions = ['t1', 't2', 't3', 't4']
     net5_init_marking = np.array([4, 0, 1, 0, 0, 0, 0, 0])
+    net5_tokens_type = ['red','green','blue']
     net5_incidence_matrix = np.array([[-1, 1, -1, 0, 0, 0, 0, 0],
                                       [0, -1, 1, 1, 1, 1, 0, 0],
                                       [0, 0, -1, 0, 0, -1, 1, 0],
@@ -19,6 +20,7 @@ def main():
     net5 = PetriNet(net5_places, net5_transitions, net5_init_marking)
     net5.set_incidence_matrix(net5_incidence_matrix)
     net5.set_arcs_incidence_matrix()
+    net5.set_tokens_type(net5_tokens_type)
     net5.plot('net5', True)
     net5.run_net()
 
