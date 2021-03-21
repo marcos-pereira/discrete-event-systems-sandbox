6178bbd86bb8db8a6d984e6e8e1fa94a8ae97d8f
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-03-21 20:14:55 -0300

    Fix petri net of task 2

--- petri_nets/main_final_project.py
+++ petri_nets/main_final_project.py
@@ -9,16 +9,18 @@ def main():
     show_output = True
 
     ## TASK 2
-    net5_places = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7']
-    net5_transitions = ['t1', 't2', 't3']
-    net5_init_marking = np.array([4, 0, 1, 0, 0, 0, 0])
-    net5_incidence_matrix = np.array([[-1, 1, -1, 0, 0, 0, 0],
-                                      [0, -1, 1, 1, 1, 1, 0],
-                                      [0, 0, 1, 0, 0, -1, 1]])
+    net5_places = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8']
+    net5_transitions = ['t1', 't2', 't3', 't4']
+    net5_init_marking = np.array([4, 0, 1, 0, 0, 0, 0, 0])
+    net5_incidence_matrix = np.array([[-1, 1, -1, 0, 0, 0, 0, 0],
+                                      [0, -1, 1, 1, 1, 1, 0, 0],
+                                      [0, 0, -1, 0, 0, -1, 1, 0],
+                                      [0, 0, 1, 0, 0, 0, -1, 1]])
     net5 = PetriNet(net5_places, net5_transitions, net5_init_marking)
     net5.set_incidence_matrix(net5_incidence_matrix)
     net5.set_arcs_incidence_matrix()
     net5.plot('net5', True)
+    net5.run_net()
 
 if __name__ == '__main__':
 
