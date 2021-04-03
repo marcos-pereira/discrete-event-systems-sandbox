92efd4c1211b4e5a1e57b382233468882741ede1
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-04-03 20:10:50 -0300

    Update files: last work was on coaccessible part of automaton

--- automata/DeterministicFiniteAutomaton.py
+++ automata/DeterministicFiniteAutomaton.py
@@ -97,6 +97,8 @@ class DeterministicFiniteAutomaton:
 
     def reachable_automaton(self):
 
+        ## Reference: http://www.cs.um.edu.mt/gordon.pace/Research/Software/Relic/Transformations/FSA/remove-unreachable.html
+
         ## Initially, only the initial state is reachable
         reachable_states = {self.initial_state_}
         print(reachable_states)
@@ -108,11 +110,13 @@ class DeterministicFiniteAutomaton:
         while element_added:
             [reachable_states, element_added] = self.reachable_states(reachable_states)
 
+        print("Reachable states:")
         print(reachable_states)
 
         ## Update automaton states
         self.states_ = reachable_states.copy()
 
+        ## Initially, all transitions are valid
         reachable_transitions = self.transitions_.copy()
 
         ## Remove unecessary transitions
@@ -120,13 +124,14 @@ class DeterministicFiniteAutomaton:
             if transition[0] not in self.states_:
                 del reachable_transitions[transition[0]]
 
+        ## Update automaton transitions
         self.transitions_ = reachable_transitions.copy()
 
 
     def reachable_states(self, states):
 
         reachable_states = states.copy()
-        init_reachable_states = states.copy()
+        init_states = states.copy()
 
         for state in states:
             ## Get transitions of the state
@@ -139,13 +144,105 @@ class DeterministicFiniteAutomaton:
                     reachable_states.add(transition[event])
 
         ## If the size of the set changes, at least an element has been added
-        if len(reachable_states) != len(init_reachable_states):
+        if len(reachable_states) != len(init_states):
             element_added = True
         else:
             element_added = False
 
         return reachable_states, element_added
 
+    def coaccessible_automaton(self):
+
+        ## The initial state is added
+        element_added = True
+
+        ## While an elment is added to the set
+        # while element_added:
+        #     [coaccessible_states, element_added] = self.coaccessible_states(coaccessible_states)
+
+        # [coaccessible_states, element_added] = self.coaccessible_states(self.states_)
+
+        coaccessible_states = set()
+        for state in self.states_:
+            print("--------")
+            print(state)
+            if self.coaccessible_states2(state):
+                coaccessible_states.add(state)
+
+        print("Coaccessible states:")
+        print(coaccessible_states)
+
+        ## Update automaton states
+        self.states_ = coaccessible_states.copy()
+
+        ## Initially all transitions are valid
+        reachable_transitions = self.transitions_.copy()
+
+        ## Remove unecessary transitions
+        # for state in self.states_:
+        #     ## Get transitions of the state
+        #     transitions = self.transitions_[state]
+        #
+        #     ## Remove transitions that lead to removed states
+        #     for transition in transitions:
+        #         for event in self.alphabet_:
+        #             if transition[1][event] not in self.states_:
+        #                 del reachable_transitions[transition]
+
+        ## Remove unecessary transitions
+        for transition in self.transitions_.items():
+            ## Check if state is still a state of the automaton
+            ## A state may have been removed due to some automata operation
+            if transition[0] in self.states_:
+                for event in self.alphabet_:
+                    ## Check if transition next state does not exist, remove it
+                    if transition[1][event] not in self.states_:
+                        del reachable_transitions[transition[0]][event]
+
+
+        ## Update automaton transitions
+        self.transitions_ = reachable_transitions.copy()
+
+    def coaccessible_states(self, states):
+
+        coaccessible_states = set()
+        init_states = states.copy()
+
+        for state in states:
+            ## Get transitions of the state
+            transition = self.transitions_[state]
+
+            ## Check all transitions of the state
+            for event in transition:
+                ## Check if the transition leads to a marked state or if the state is marked
+                if transition[event] in self.marked_states_ or state in self.marked_states_:
+                    coaccessible_states
+
+        ## If the size of the set changes, at least an element has been added
+        if len(coaccessible_states) != len(init_states):
+            element_added = True
+        else:
+            element_added = False
+
+        return coaccessible_states, element_added
+
+    def coaccessible_states2(self, state):
+
+        ## Get transitions of the state
+        transition = self.transitions_[state]
+        print("state "+state)
+        print(transition)
+
+        ## Check all transitions of the state
+        for event in transition:
+            print(transition[event])
+            ## Check if the transition leads to a marked state or if the state is marked
+            if transition[event] in self.marked_states_ or state in self.marked_states_:
+                return True
+            elif transition[event]:
+                return False
+            else:
+                self.coaccessible_states2(transition[event])
 
 
 
--- automata/main.py
+++ automata/main.py
@@ -5,15 +5,16 @@ from DeterministicFiniteAutomaton import DeterministicFiniteAutomaton
 def main():
     print("main()...")
 
-    states = {'0','1','2','3'}
+    states = {'0','1','2','3','4'}
     transitions = {
         '0':{'a':'1','b':'0'},
         '1':{'a':'2','b':'0'},
-        '2':{},
-        '3':{'a':'2'}
+        '2':{'b':'3'},
+        '3':{'a':'2','b':'1'},
+        '4':{'b':'3'}
     }
     initial_state = '0'
-    marked_states = {'2'}
+    marked_states = {'1'}
     alphabet = {'a','b'}
     event_sequence = 'aaba'
 
@@ -28,9 +29,34 @@ def main():
     dfa.plot_automaton('automaton1', show_output)
 
     ## Convert automaton to get accessible part
-    dfa.reachable_automaton()
+    # dfa.reachable_automaton()
+    # dfa.print_automaton()
+    # dfa.plot_automaton('reachable_automaton', show_output)
+
+    ## Convert automaton to get accessible part
+    # dfa.coaccessible_automaton()
+    # dfa.print_automaton()
+    # dfa.plot_automaton('coaccessible_automaton', show_output)
+
+    states = {'on', 'off'}
+    transitions = {
+        'on': {'turn_off': 'off'},
+        'off': {'turn_on': 'on'},
+    }
+    initial_state = 'off'
+    marked_states = {'on'}
+    alphabet = {'turn_on', 'turn_off'}
+    event_sequence = 'turn_on'+'turn_off'
+
+    dfa = DeterministicFiniteAutomaton(states, transitions, initial_state, marked_states, alphabet)
+
+    ## Show automaton plot output
+    show_output = True
+
+    ## Print automaton
     dfa.print_automaton()
-    dfa.plot_automaton('reachable_automaton', show_output)
+    # dfa.run(event_sequence)
+    dfa.plot_automaton('light_bulb', show_output)
 
 if __name__ == '__main__':
 
