7fabaf3c06e44b544d7b8d72c576930f69c96b9a
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-04-09 17:04:08 -0300

    Add several automata examples to be print

--- automata/main.py
+++ automata/main.py
@@ -29,14 +29,17 @@ def main():
     dfa.plot_automaton('automaton1', show_output)
 
     ## Convert automaton to get accessible part
-    # dfa.reachable_automaton()
-    # dfa.print_automaton()
-    # dfa.plot_automaton('reachable_automaton', show_output)
+    dfa.reachable_automaton()
+    dfa.print_automaton()
+    dfa.plot_automaton('reachable_automaton', show_output)
 
     ## Convert automaton to get accessible part
+    # TODO: finish the implementation of the coaccessible part
     # dfa.coaccessible_automaton()
     # dfa.print_automaton()
     # dfa.plot_automaton('coaccessible_automaton', show_output)
+
+    ## Below are many examples of automata
 
     # Light bulb automaton
     states = {'on', 'off'}
@@ -47,7 +50,6 @@ def main():
     initial_state = 'off'
     marked_states = {'on'}
     alphabet = {'turn_on', 'turn_off'}
-    event_sequence = 'turn_on'+'turn_off'
 
     dfa = DeterministicFiniteAutomaton(states, transitions, initial_state, marked_states, alphabet)
 
@@ -56,7 +58,6 @@ def main():
 
     ## Print automaton
     dfa.print_automaton()
-    # dfa.run(event_sequence)
     dfa.plot_automaton('light_bulb', show_output)
 
     ## Client queue
@@ -77,7 +78,6 @@ def main():
 
     ## Print automaton
     dfa.print_automaton()
-    # dfa.run(event_sequence)
     dfa.plot_automaton('queue', show_output)
 
     ## Client server
@@ -97,8 +97,64 @@ def main():
 
     ## Print automaton
     dfa.print_automaton()
-    # dfa.run(event_sequence)
     dfa.plot_automaton('server', show_output)
+
+    ## Machine 1
+    states = {'M1_available', 'M1_working'}
+    transitions = {
+        'M1_available': {'receive_piece': 'M1_working'},
+        'M1_working': {'return_piece': 'M1_available'},
+    }
+    initial_state = 'M1_available'
+    marked_states = {'M1_available'}
+    alphabet = {'receive_piece', 'return_piece'}
+
+    dfa = DeterministicFiniteAutomaton(states, transitions, initial_state, marked_states, alphabet)
+
+    ## Show automaton plot output
+    show_output = True
+
+    ## Print automaton
+    dfa.print_automaton()
+    dfa.plot_automaton('machine1', show_output)
+
+    ## Machine 2
+    states = {'M2_available', 'M2_working'}
+    transitions = {
+        'M2_available': {'receive_piece': 'M2_working'},
+        'M2_working': {'return_piece': 'M2_available'},
+    }
+    initial_state = 'M2_available'
+    marked_states = {'M2_available'}
+    alphabet = {'receive_piece', 'return_piece'}
+
+    dfa = DeterministicFiniteAutomaton(states, transitions, initial_state, marked_states, alphabet)
+
+    ## Show automaton plot output
+    show_output = True
+
+    ## Print automaton
+    dfa.print_automaton()
+    dfa.plot_automaton('machine2', show_output)
+
+    ## Robot
+    states = {'available', 'handling_M1', 'handling_M2'}
+    transitions = {
+        'available': {'receive_piece': 'M2_working'},
+        'handling_M1': {'return_piece': 'M2_available'},
+    }
+    initial_state = 'M2_available'
+    marked_states = {'M2_available'}
+    alphabet = {'receive_piece', 'return_piece'}
+
+    dfa = DeterministicFiniteAutomaton(states, transitions, initial_state, marked_states, alphabet)
+
+    ## Show automaton plot output
+    show_output = True
+
+    ## Print automaton
+    dfa.print_automaton()
+    dfa.plot_automaton('machine2', show_output)
 
 if __name__ == '__main__':
 
